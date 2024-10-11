53f9867917ed84fea324c4871e72f2020f485cbc
hks88 <hksptysoporte@gmail.com>
2024-10-11 13:20:15 -0500

    global counter ADDED

---
+++ autoScroll.mjs
@@ -0,0 +1,19 @@
+// Función para realizar el scroll hasta el final de la página
+export async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      const distance = 100;
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
--- scrapper.mjs
+++ scrapper.mjs
@@ -2,98 +2,81 @@
 
 import { chromium } from "playwright";
 import fs from "fs";
-
-// Arreglo de URLs a las que quieres hacer scraping
-const urls = ["https://www.superxtra.com/supermercado/bebidas-y-jugos?page=1"];
+import { autoScroll } from "./autoScroll.mjs";
 
 const browser = await chromium.launch({
-  headless: false, // Cambiar a false para ver el proceso en la ventana del navegador
+  headless: false,
 });
 
 const page = await browser.newPage();
 
-try {
-  // Array para almacenar todas las URLs de imágenes de cada página
-  let allProducts = [];
+// ------------------- URLS ------------------- //
+const urls = [
+  "https://www.superxtra.com/supermercado/bebidas-y-jugos?page=1",
+  "https://www.superxtra.com/supermercado/bebidas-y-jugos?page=2",
+  "https://www.superxtra.com/supermercado/bebidas-y-jugos?page=3",
+  "https://www.superxtra.com/supermercado/bebidas-y-jugos?page=4",
+];
 
-  // Iterar sobre cada URL
+try {
+  let allData = [];
+  let idCounter = 1; // Contador global de IDs
+
   for (const url of urls) {
     console.log(`Visitando: ${url}`);
-
     await page.goto(url, {
-      waitUntil: "networkidle", // Esperar hasta que se haya cargado la mayoría de los recursos
+      waitUntil: "networkidle",
     });
-
-    // Realizar scroll hasta el final de la página
     await autoScroll(page);
+    await page.waitForTimeout(7000);
 
-    // Esperar 10 segundos para asegurarse de que todo el contenido dinámico se haya cargado
-    await page.waitForTimeout(10000);
-
-    // Esperar explícitamente a que las imágenes de los productos se carguen en la página
+    // ------------------- WAIT FOR THIS CLASS TO RENDER ------------------- //
     await page.waitForSelector(".vtex-search-result-3-x-gallery section img");
 
-    // Ahora proceder con la extracción de los productos
-    const products = await page.$$eval(
+    const data = await page.$$eval(
+      // ------------------- PARENT CLASS OF DATA TO BE SCRAPED ------------------- //
       ".vtex-product-summary-2-x-container",
-      (results) =>
-        results.map((el, index) => {
-          // Inicializar el objeto de producto con el ID
-          const product = {
-            id: index + 1, // Agregar un id basado en el índice del mapeo (empezando desde 1)
-          };
+      (results, idCounter) =>
+        results.map((el) => {
+          // ------------------- ID ------------------- //
+          const num = idCounter++;
 
-          // Extraer el atributo href del enlace; Concatenar con la URL base si existe el enlace
-          const relativeUrl = el.querySelector("a")?.getAttribute("href");
-          product.link = relativeUrl
-            ? `https://www.superxtra.com${relativeUrl}`
-            : null;
-
-          // Extraer el contenido del span del nombre del producto
+          // ------------------- NAME ------------------- //
           const productName = el
             .querySelector(
               ".vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName"
             )
             ?.textContent?.trim();
-          product.name = productName;
 
-          return product;
-        })
+          // ------------------- LINK ------------------- //
+          const productLink =
+            el.querySelector("a")?.getAttribute("href") &&
+            `https://www.superxtra.com${el
+              .querySelector("a")
+              .getAttribute("href")}`;
+
+          // ------------------- JSON ELEMENTS ------------------- //
+          const json = {
+            id: num,
+            name: productName,
+            link: productLink,
+          };
+
+          return json;
+        }),
+      idCounter // Pasar el contador actual como argumento a la función de evaluación
     );
 
-    // Filtrar productos válidos (que tengan al menos un campo válido) y agregar a la lista general
-    const validProducts = products.filter(
-      (product) => product.link && product.name
-    );
+    // Actualizar el contador global para el siguiente lote
+    idCounter += data.length;
 
-    allProducts.push(...validProducts);
+    allData.push(...data);
   }
 
-  // Guardar todos los productos en un archivo JSON sin eliminar duplicados
-  const jsonContent = JSON.stringify({ products: allProducts }, null, 2); // Formatear el JSON con indentación
-  fs.writeFileSync("products.json", jsonContent);
+  const jsonContent = JSON.stringify({ data: allData }, null, 2);
+  fs.writeFileSync("data.json", jsonContent);
 
-  console.log("Datos de productos guardados en products.json");
+  console.log("Scrapping Completado ✔️");
 } finally {
   await browser.close();
-}
-
-// Función para realizar el scroll hasta el final de la página
-async function autoScroll(page) {
-  await page.evaluate(async () => {
-    await new Promise((resolve) => {
-      let totalHeight = 0;
-      const distance = 100; // Distancia a desplazar en cada iteración
-      const timer = setInterval(() => {
-        const scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 100); // Esperar 100ms entre cada desplazamiento
-    });
-  });
 }
